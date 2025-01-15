2bd71cc45b312c717615f4b3afa2393327518d09
Angel Arevalo <angelarevalo480@gmail.com>
2025-01-15 19:21:07 -0400

    Javascript 3
    El dia de hoy realizamos mas ejercicios, se revisaron los ejercicios de operadores en general
    
    Se enviaron 2 ejercicios para practicar.

---
+++ JavaScript/ejercicios.js
@@ -0,0 +1,22 @@
+// Ejecicio para practicar 
+
+
+/*
+  1. Calculadora V0
+  Crea tres variables, A, B y un "operacion". 
+  Asignarles un valor a cada uno
+
+  Crea una condicion que verifique si el operador es +, -, *, /, %, **.
+  Dependiendo del operador, realiza la operacion correspondiente con los dos numeros.
+  Debes imprimir por consola el resultado.
+
+  Opcional: hacer este ejercicio en DOM 
+  (A, B y "operacion" deben ser ingresadas por el usuario y mostrar el resultado con alert)
+
+  2. Meses del año
+  Crea una variable llamada mes (numero) que imprima en consola el mes (letras).
+  
+  Opcional: hacer este ejercicio en DOM.
+
+  Fecha de entrega: 16/01/2025
+*/
