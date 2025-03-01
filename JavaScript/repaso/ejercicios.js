20bf1c9e81aad7b17fbbac3cd64bc04de37d6df8
Angel Arevalo <angelarevalo480@gmail.com>
2025-02-28 20:01:22 -0400

    Ejercicios Resueltos JS
    Co-authored-by: jmtovar1022 <jmtovar1022@users.noreply.github.com>
    Co-authored-by: fareyesp2003 <fareyesp2003@users.noreply.github.com>
    Co-authored-by: Héctor J. Rodríguez <hectorjrp78@users.noreply.github.com>

--- JavaScript/repaso/ejercicios.js
+++ JavaScript/repaso/ejercicios.js
@@ -191,14 +191,6 @@ console.log(calcularImpuestos(30, 500));
 
 // ---------------- Brenyer -------------------
 
-
-
-
-
-
-
-
-
 // 7. FizzBuzz
 
 // Escribir una función llamada `fizzBuzz` que reciba un número y
@@ -214,6 +206,7 @@ console.log(calcularImpuestos(30, 500));
 // console.log(fizzBuzz(20)); // "buzz"
 // console.log(fizzBuzz(30)); // "fizzbuzz"
 // console.log(fizzBuzz(8)); // 8
+
 // -------------------- Hector ----------------
 
 function fizzBuzz (numero){
@@ -238,11 +231,6 @@ console.log(fizzBuzz(8));
 // 8. Contar rango de números
 
 
-function contarRango(a,b){
-  rango = b-a -2; 
-  console.log("El rango entre los números es: ", rango); 
-}
-
 // Escribir una función llamada `contarRango` que reciba dos 
 // números y retorne cuántos números que hay entre ellos (excluyéndolos):
 
@@ -256,11 +244,21 @@ function contarRango(a,b){
 
 //  ----------------- Jose ----------------
 
+function contarrango(a,b){
+  let suma = 0;
+  for(let i=(a+1); i<=(b-1); i++){
+    suma += i;
+  }
+  }
+  return suma; 
 
 
+console.log(contarrango(1,9));
+console.log(contarrango(1332,8743));
+console.log(contarrango(5,6));
 
 
-// Transcribir ADN a ARN
+// 9. Transcribir ADN a ARN
 
 // Escribir una función llamada `transcribir` 
 // que reciba un string (una cadena de ADN) y retorne otro string (su complemento ARN).
@@ -275,6 +273,7 @@ function contarRango(a,b){
 // // código de prueba
 // console.log(transcribir("ACGT")) -> "UGCA"
 // console.log(transcribir("ACGTGGTCTTAA")) -> "UGCACCAGAAUU"
+// -------------- Glayre ----------------
 
 function transcribir(ADN){
   let arn= "";
@@ -298,13 +297,12 @@ function transcribir(ADN){
 console.log(transcribir("ACGT"));
 console.log(transcribir("ACGTGGTCTTAA"));
 
-// -------------- Glayre ----------------
 
 
 
 
 
-// 20. Generar contraseña
+// 10. Generar contraseña
 
 // Escribir una función llamada `password` que reciba un string y 
 // retorne un nuevo string realizando los siguientes cambios:
@@ -343,3 +341,197 @@ function password(clave){
 };
 
 console.log(password("hoLa Soy una Clave"));
+
+
+// 11. Encontrar números pares en un arreglo
+
+// Escribir una función llamada `pares` que reciba un arreglo de números
+//  y retorne un nuevo arreglo con los números pares únicamente.
+
+// código de prueba
+// console.log(pares([1, 2, 3, 4, 5, 6])) -> [2, 4, 6]
+// console.log(pares([])) -> []
+
+function pares(arreglo){
+  let paresArreglo = [];
+  for (let i = 0; i < arreglo.length; i++){
+    if (arreglo[i] % 2 == 0){
+      paresArreglo.push(arreglo[i])
+    } 
+  }
+  return paresArreglo 
+}
+console.log(pares([1, 2, 3, 4, 5, 6]))
+console.log(pares([100,45,12,7,1,0,53,82]))
+
+// 12. Traducir números a palabras
+
+// Escribir una función llamada `numerosAPalabras` que 
+// reciba un arreglo de números (cada número en el rango de 0 a 10)
+//  y retorne un nuevo arreglo convirtiendo cada número a su versión en palabras.
+
+// código de prueba
+// console.log(numerosAPalabras([0, 1, 2, 3, 4])) -> ["cero", "uno", "dos", "tres", "cuatro"]
+// console.log(numerosAPalabras([5, 6, 7, 8, 9])) -> ["cinco", "seis", "siete", "ocho", "nueve"]
+
+function numerosApalabras(numeros){
+  let palabras = [];
+  for (let i= 0; i < numeros.length; i++){
+    switch (numeros[i]) {
+    case 0:
+        palabras.push("cero");
+        break;
+    case 1:
+        palabras.push("uno");
+        break;
+    case 2:
+        palabras.push("dos");
+        break;
+    case 3:
+        palabras.push("tres");
+        break;
+    case 4:
+        palabras.push("cuatro");
+        break;
+    case 5:
+        palabras.push("cinco");
+        break;
+    case 6:
+        palabras.push("seis");
+        break;
+    case 7:
+        palabras.push("siete");
+        break;
+    case 8:
+        palabras.push("ocho");
+        break;
+    case 9:
+        palabras.push("nueve");
+      break;
+    case 10:
+        palabras.push("diez");
+      break;
+    default:
+        palabras.push("N/A");
+    }
+  }
+  return palabras
+}
+console.log(numerosApalabras([5,0, 1, 2.2, 3, 4, "hola", 6, 7, 8, 9, 10]));
+
+
+// ---------------- Glayre ----------------
+
+
+// 13. Contraseña válida
+
+// Escribir una función llamada `contrasenaValida` que reciba un string 
+// y retorne `true` si el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9".
+//  De lo contrario debe retornar `false`.
+
+// código de prueba
+// console.log(contrasenaValida("2Fj(jjbFsuj")) // true
+// console.log(contrasenaValida("eoZiugBf&g9")) // true
+// console.log(contrasenaValida("hola")) // false
+// console.log(contrasenaValida("")) // false
+// ----------------- Sergio ----------------
+
+// Para recibir informacion por consola debemos utilizar inp
+
+function contrasenaValida(password){
+  if(password == "2Fj(jjbFsuj" || password == "eoZiugBf&g9"){
+      return true
+  }else{
+      return false
+  }
+}
+
+let palabra = input("Ingrese una palabra: ");
+console.log(contrasenaValida(palabra));
+
+
+// 14. Encontrar palabras que empiecen por "a"
+
+// Escribir una función llamada `empiezanConA` que reciba un arreglo
+//  de strings y retorne un nuevo arreglo con todas las palabras que
+// empiecen por "a" (mayúscula o minúscula).
+
+// código de prueba
+// console.log(empiezanConA(["beta", "alfa", "Arbol", "gama"])) // ["alfa", "Arbol"]
+// console.log(empiezanConA(["beta", "delta", "gama"])) // []
+// console.log(empiezanConA([])) // []
+// palabra[i][0] -> primera letra
+// ------------ Fernando ----------------
+
+function empiezanConA(arreglo){ 
+  let resultado = [];
+  for (let i = 0; i < arreglo.length; i++)
+  {
+    if(arreglo[i][0].toLowerCase() == "a") {
+     resultado.push(arreglo[i]); 
+    }
+  }
+  return resultado
+}
+console.log(empiezanConA(["beta", "alfa", "Arbol", "gama"])) // ["alfa", "Arbol"]
+console.log(empiezanConA(["beta", "delta", "gama"])) // []
+console.log(empiezanConA([])) // []
+// palabra[i][0] -> primera letra
+
+
+
+
+// 15. Encontrar el número máximo
+
+// Escribir una función llamada `max` que reciba un arreglo
+//  de números y retorne el número máximo:
+
+// **Nota:** Intentarlo hacer sin el método `Math.max` de JavaScript.
+
+// código de prueba
+// console.log(max([3, 9, 6])) // 9
+// console.log(max([67, 35, 54, 26])) // 67
+// console.log(max([5, 9, 2, 4, 5, 7])) // 9
+// ------------- Sergio ----------------
+
+function max(array){
+  let max = array[0];
+  for(let i = 1; i < array.length; i++){
+      if(array[i] > max){
+          max = array[i]
+      }
+  }
+  return max
+}
+console.log(max([3, 9, 6]))
+console.log(max([67, 35, 9999954, 26]))
+console.log(max([5, 9, 2, 4, 5, 7]))
+
+
+
+// 16. Encontrar palabras que terminan en "s"
+
+// Escribir una función llamada `terminanConS` que reciba un arreglo
+//  de strings y retorne un nuevo arreglo con todas las palabras que
+//  terminan con "s" (mayúscula o minúscula).
+
+// código de prueba
+// console.log(terminanConS(["pruebas", "arroz", "árbol", "tokens"])) // ["pruebas", "tokens"]
+// console.log(terminanConS(["beta", "delta", "gama"])) // []
+// console.log(terminanConS([])) // []
+// ultimo elemento -> palabra.length - 1
+
+// ------------- Sergio ----------------
+
+function terminanConS(array){
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+      if(array[i][array[i].length - 1] == "s" || array[i][array[i].length - 1] == "S"){
+          newArray.push(array[i])
+      }
+  }
+  return newArray
+}
+console.log(terminanConS(["pruebas", "arroz", "árbol", "tokens"]))
+console.log(terminanConS(["betaS", "sdelsta", "gamas"]))
+console.log(terminanConS([]))
