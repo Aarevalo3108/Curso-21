f897074f223412dbfd2e42fb5af848a803a0c4d7
juanert <61994784+juanert@users.noreply.github.com>
2025-04-21 19:51:58 -0400

    Juan 1

--- React/src/components/pages/Home.jsx
+++ React/src/components/pages/Home.jsx
@@ -1,19 +1,21 @@
-import { useState } from "react"
+import { useState } from "react";
 // import Gato from "./gato.png" <- si se encuentra en la carpeta assets
 
 const Home = () => {
-
-  const [contador, setContador] = useState(0)
+  const [contador, setContador] = useState(0);
 
   return (
     <>
       <h1 className="text-4xl text-center">Home</h1>
       <img src="./gato.png" alt="Gato" />
-      <button className="text-4xl bg-blue-600 rounded-2xl p-4" onClick={() => setContador(contador + 1)}>
+      <button
+        className="text-4xl bg-blue-600 rounded-2xl p-4"
+        onClick={() => setContador(contador + 1)}
+      >
         <p>{contador}</p>
       </button>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- React/src/components/pages/Services.jsx
+++ React/src/components/pages/Services.jsx
@@ -1,13 +1,9 @@
-
-
-
-
 const Services = () => {
   return (
     <div>
       <h1 className="text-4xl text-center">Services</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
